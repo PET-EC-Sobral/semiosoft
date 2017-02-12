47f8d66b9bc61303f5265b14f6d43afd4c9584fd
Laercio Santana <laerciosantana021@hotmail.com>
2017-02-12 11:02:34 -0300

    [osteomuscular] mudanças na implementação da tabela

--- src/pages/osteo/pages/o8.ts
+++ src/pages/osteo/pages/o8.ts
@@ -11,8 +11,29 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'o8.html'
 })
 export class O8 {
+	table: Array<Array<string> >;
 
-  constructor(public navCtrl: NavController) {}
+	constructor(public navCtrl: NavController) {
+		this.populateTable();
+	}
+
+	populateTable(){
+	  	this.table = [
+	  		["Manobras", "Avaliação", "Descrição",],
+	  		[
+	  			`<b>Tinel</b>`,
+	  			`Síndrome do Túnel do Tarso`,
+	  			`Percussão na região posterior ao maléolo medial com
+	  			reprodução de dor e parestesia para os primeiros pododáctilos`
+	  		],
+	  		[
+	  			`<b>Thompson</b>`,
+	  			`Integridade do tendão de Aquiles`,
+	  			`Paciente em decúbito ventral na mesa de exames com os pés para fora. O examinador realiza a
+		          compressão da panturrilha levando uma discreta flexão plantar.`
+	  		]
+	  	];
+  }
 
 
 }
--- src/pages/osteo/pages/o9.ts
+++ src/pages/osteo/pages/o9.ts
@@ -11,8 +11,76 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'o9.html'
 })
 export class O9 {
+  table: Array<Array<string> >;
+  constructor(public navCtrl: NavController) {
+  	this.populateTable();
+  }
+  populateTable(){
+  	this.table = [
+  		["Tipos de manobra", "Avaliação", "Descrição",],
 
-  constructor(public navCtrl: NavController) {}
-
+  		[
+  		"<b>Manobra de Jobe</b>", 
+  		`Paciente com lesão do manguito rotador
+	     queixa-se de dor na região anterior do ombro`,
+	     `Braço em abdução de 90º graus rotaclo
+	      internamente e antebraço pronado com
+	      polegar para baixo. O examinador força o
+	      movimento de descida dos braços contra
+	      resistência do paciente.`
+	      ],
+	    [
+	      `<b>Manobra de Yergason</b>`,
+	      `Paciente com tendinite da cabeça longa do
+	        bíceps apresenta dor na região anterior do
+	        ombro`,
+	        `Paciente com os cotovelos em flexão de 90º o
+	        examinador provoca uma supinação forçada
+	        contra resistência`
+	    ],
+	    [
+	    	`<b>Arco doloroso de Simmonds</b>`,
+	    	`Avalia os processos inflamatórios que afetam o
+	          tendão do músculo supra-espinhal e as bolsas
+	          sinoviais do ombro`,
+	    	`A manobra consiste na elevação ativa do
+	        ombro no plano da escápula com o membro
+	        em rotação medial.A resposta e' positiva
+	        quando há dor entre 70º e 120º`
+	    ],
+	    [
+	    	`<b>Manobra de Yokum</b>`,
+	    	`Avalia tanto a artrose acrômio-clavicular como
+	        tendinopatia do supraespinhoso`,
+	    	`O paciente apoia a mão no ombro
+	        contralateral, enquanto passivamente eleva-se
+	        o membro pelo cotovelo.`
+	    ],
+	    [
+	    	`<b>Manobra de Tinnel</b>`,
+	    	`Sindrome do Túnel do Carpo`,
+	    	`Realiza-se a percussão da região ventral do
+	        punho, quando positivo o paciente refere dor
+	        e parestesia sendo transmitida ao território de
+	        inervação do nervo mediano.`
+	    ],
+	    [
+	    	`<b>Manobra de Phalen</b>`,
+	    	`Síndrome do Túnel do Carpo`,
+	    	`Reprodução de parestesia do primeiro até
+	          metade do quarto quirodáctilo mediante a
+	          flexão máxima do punho e a sua manutenção
+	          pelo período de 1 minuto.`
+	    ],
+	    [
+	    	`<b>Manobra de Finkelstein</b>`,
+	    	`Tendinite de Quervain`,
+	    	`Paciente com a mão fechada e o polegar
+	        aduzido e fletido, realiza um desvio ulnar
+	        apresenta dor na face lateral da mão e do
+	        punho.`
+	    ]
+  	];
+  }
 
 }
